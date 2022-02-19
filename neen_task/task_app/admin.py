3c9a0e5d22558c69d653eabd1416fb186c607e05
apurbamandal720 <48640699+apurbamandal720@users.noreply.github.com>
2022-02-19 09:00:18 +0530

    Delete neen_task/task_app directory

--- neen_task/task_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from task_app.models import *
-# Register your models here.
-
-@admin.register(Blog)
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'content', 'is_published')
